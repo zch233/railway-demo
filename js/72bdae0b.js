b4f0f8b6f03978bd9d576c477dd5f036fdd1edd7
zhangchao <529743595@qq.com>
2022-12-04 22:13:55 +0800

    deploy

---
+++ js/72bdae0b.js
@@ -0,0 +1,1 @@
+import{ck as r,cl as s,h as a}from"./9b963d80.js";const c=r("app",()=>{const e=s("orderList",[]),t=o=>e.value=o;return{dataSource:a(()=>({list:e.value,total:e.value.length})),setOrderList:t,orderList:e}});export{c as u};
